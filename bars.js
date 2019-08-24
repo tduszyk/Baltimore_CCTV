f8a4acdc12bc8565eee4927baa1a6e931d4b6b53
Tom Duszyk <tduszyk@gmail.com>
2019-08-24 13:11:15 -0400

    added crime count to js script

--- bars.js
+++ bars.js
@@ -8,7 +8,7 @@ var crime = "https://data.baltimorecity.gov/resource/h32e-c3r6.json"
 d3.json(cctv, function (data) { //pass the full JSON link
   neighborhoods_cctv = [];
 
-  data.forEach(function (element) {  //filter for lat and long with foreach loop
+  data.forEach(function (element) { 
     neighborhoods_cctv.push(element.neighborhood.toUpperCase());
     //return(neighborhoods_cctv);
   });
@@ -17,24 +17,32 @@ d3.json(cctv, function (data) { //pass the full JSON link
   d3.json(crime, function (data) {
     neighborhoods_crimes = [];
 
-
     data.forEach(function (element) {
       neighborhoods_crimes.push(element.neighborhood.toUpperCase());
+    //console.log(neighborhoods_crimes);
+    });
 
-    })
-  })
+    // Gettting Cameras per Neighborhood
+    var counts = {};
+    for (var i = 0; i < neighborhoods_cctv.length; i++) {
+      counts[neighborhoods_cctv[i]] = 1 + (counts[neighborhoods_cctv[i]] || 0);
+    };
+    //console.log(neighborhoods_cctv);
 
-  // Gettting Cameras per Neighborhood
-  var counts = {};
-  for (var i = 0; i < neighborhoods_cctv.length; i++) {
-    counts[neighborhoods_cctv[i]] = 1 + (counts[neighborhoods_cctv[i]] || 0);
-  };
+    // Getting Crimes per Neighborhood
+    var crimeN = {};
+    for (var j = 0; j < neighborhoods_crimes; j++) {
+      crimeN[neighborhoods_crimes[j]] = 1 + (crimeN[neighborhoods_crimes[j]] || 0);
+    };
+    //console.log(neighborhoods_crimes);
 
-  // Creating list of dictionaries
-  var camerasNeighborhood = [];
-  for (let [key, value] of Object.entries(counts)) {
-    // console.log(`${key}: ${value}`);
-    camerasNeighborhood.push({ Neighborhood: `${key}`, NumberOfCameras: `${value}` });
-  }
-  console.log(camerasNeighborhood);
-});
+    // Creating list of dictionaries
+    var camerasNeighborhood = [];
+    for (let [key, value] of Object.entries(counts)) {
+      // console.log(`${key}: ${value}`);
+      camerasNeighborhood.push({ Neighborhood: `${key}`, NumberOfCameras: `${value}` });
+    }
+    //console.log(camerasNeighborhood);
+  
+  });
+})
