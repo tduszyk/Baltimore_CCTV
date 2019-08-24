26a75aae26f35fe4f6bc9e4a37b0b6a5e8106b10
Tom Duszyk <tduszyk@gmail.com>
2019-08-24 13:40:47 -0400

    commit

--- bars.js
+++ bars.js
@@ -2,7 +2,7 @@
 var cctv = "https://data.baltimorecity.gov/resource/h32e-c3r6.json"
 
 //Baltimore Crime API endpoint
-var crime = "https://data.baltimorecity.gov/resource/h32e-c3r6.json"
+var crime = "https://data.baltimorecity.gov/resource/wsfq-mvij.json"
 
 // Grabbing json data for CCTV
 d3.json(cctv, function (data) { //pass the full JSON link
@@ -18,7 +18,7 @@ d3.json(cctv, function (data) { //pass the full JSON link
     neighborhoods_crimes = [];
 
     data.forEach(function (element) {
-      neighborhoods_crimes.push(element.neighborhood.toUpperCase());
+      neighborhoods_crimes.push(element.neighborhood);
     //console.log(neighborhoods_crimes);
     });
 
@@ -31,18 +31,25 @@ d3.json(cctv, function (data) { //pass the full JSON link
 
     // Getting Crimes per Neighborhood
     var crimeN = {};
-    for (var j = 0; j < neighborhoods_crimes; j++) {
+    for (var j = 0; j < neighborhoods_crimes.length; j++) {
       crimeN[neighborhoods_crimes[j]] = 1 + (crimeN[neighborhoods_crimes[j]] || 0);
     };
-    //console.log(neighborhoods_crimes);
+    console.log(crimeN);
 
-    // Creating list of dictionaries
+    // Creating list of dictionaries for CCTV
     var camerasNeighborhood = [];
     for (let [key, value] of Object.entries(counts)) {
       // console.log(`${key}: ${value}`);
       camerasNeighborhood.push({ Neighborhood: `${key}`, NumberOfCameras: `${value}` });
     }
     //console.log(camerasNeighborhood);
+
+    // Create list of dictionaries for Crimes
+    // var crimesNeighborhood = [];
+    // for (let [key, value] of Object.entries(crimeN)) {
+    //   crimesNeighborhood.push({ Neighborhood: `${key}`, NumberOfCrimes: `${value}` });
+    // }
+    // console.log(crimesNeighborhood);
   
   });
 })
